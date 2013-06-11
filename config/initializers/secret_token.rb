13cad4343eb4c452f1348340a30d48e1022aa67e
Paul Sutcliffe <paul@kosmyka.com>
2013-06-10 22:51:43 -0500

    Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Globalvet::Application.config.secret_token = '7c0110e3c5fba7647f5cbbeb6e70d5b512971c3659b8e460eafd1f84aed19107875af3f5df61ef089ad0561e7ccf5865d11e55ce9271db10fd6a007bc0f579bc'
