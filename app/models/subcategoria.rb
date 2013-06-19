dc185f186bd071b8a27d72af135eed741c5d9302
Paul Sutcliffe <paul@kosmyka.com>
2013-06-18 22:41:39 -0500

    Implementados los indexes de productos

--- app/models/subcategoria.rb
+++ app/models/subcategoria.rb
@@ -4,7 +4,6 @@ class Subcategoria < ActiveRecord::Base
   friendly_id :nombre, use: :slugged
 
   has_many :tipos
-  has_many :productos
   belongs_to :categoria
 
   mount_uploader :imagen, ImagenUploader
