0a36b7556dab28602969380d5d579e4a7f5f9df9
Paul Sutcliffe <paul@kosmyka.com>
2013-07-22 00:10:57 -0500

    Implementado el link de noticias

--- app/models/noticia.rb
+++ app/models/noticia.rb
@@ -1,5 +1,5 @@
 class Noticia < ActiveRecord::Base
-  attr_accessible :contenido, :imagen, :titulo, :slug
+  attr_accessible :contenido, :imagen, :titulo, :slug, :link
   extend FriendlyId
   friendly_id :titulo, use: :slugged
 
